d4d3432513d7c3cc5f316ea7566208f737a93b34
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-02 20:41:09 +0100

    clean up computeNextGeneration

--- src/game-of-life.js
+++ src/game-of-life.js
@@ -23,21 +23,14 @@ const countAliveNeighbours = (arr, i, j) => {
 const computeNextGeneration = (arr, i, j) => {
   const numberOfLivingNeighbours = countAliveNeighbours(arr, i, j);
 
-  if (arr[i][j] === 1) {
-    if (numberOfLivingNeighbours < 2 || numberOfLivingNeighbours > 3) {
-      return 0;
-    }
-
-    if (numberOfLivingNeighbours === 2 || numberOfLivingNeighbours === 3) {
-      return 1;
-    }
-  } else {
-    if (numberOfLivingNeighbours === 3) {
-      return 1;
-    } else {
-      return 0;
-    }
+  if (
+    numberOfLivingNeighbours === 3 ||
+    (numberOfLivingNeighbours === 2 && arr[i][j] === 1)
+  ) {
+    return 1;
   }
+
+  return 0;
 };
 
 const gameOfLife = (arr) => {
@@ -55,8 +48,3 @@ const gameOfLife = (arr) => {
 };
 
 module.exports = { gameOfLife };
-
-// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-// Any live cell with two or three live neighbours lives on to the next generation.
-// Any live cell with more than three live neighbours dies, as if by overpopulation.
-// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
